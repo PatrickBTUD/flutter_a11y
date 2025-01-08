86419a38b96463089be1e80e12fd1c622258cbf0
Patrick Buchholz <patrickbuchholz@appsfactory.de>
2025-01-08 11:54:54 -0300

    [ADD] AppBar, FAB

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,19 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          //title is used by screen readers to tell about heading
+          title: const Text('Flutter A11Y App'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          //tooltip is used by screen readers to describe the button
+          tooltip: 'Increment',
+          onPressed: () {
+            debugPrint('FloatingActionButton pressed');
+          },
+        ),
         body: Center(
           child: Text('Hello World!'),
         ),
